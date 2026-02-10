5af3226d72c0b3af9d8681c0c21b153807afea2d
Tashi Wangchuk <tashiwangchuk619@gmail.com>
2026-02-10 15:02:37 +0600

    initial code base - spring boot

---
+++ src/main/java/com/lunchbox/lunch_box/LunchBoxApplication.java
@@ -0,0 +1,13 @@
+package com.lunchbox.lunch_box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lunchbox/lunch_box/LunchBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunchbox.lunch_box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
